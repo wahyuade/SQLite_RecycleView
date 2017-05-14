4abbc6f61a299e78816f9d228b521d1e5dfff579
Wahyu Ade Sasongko <wahyuadesasongko@gmail.com>
2017-05-14 21:10:19 +0100

    fresh project

---
+++ app/src/main/java/recycleview/sqlite/wahyuadesasongko/sqlite_with_recycleview/MainActivity.java
@@ -0,0 +1,13 @@
+package recycleview.sqlite.wahyuadesasongko.sqlite_with_recycleview;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
